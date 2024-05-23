44204f162725d89d59ba0c91e3b5bc6d22cd7a30
Gde <gugumgde26@gmail.com>
2024-05-23 19:36:04 +0700

    Mantab

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      outfit: ["'Outfit'", "sans-serif"],
+      texturina: ["'Texturina'", "serif"],
+    }
+  },
+  plugins: [],
+}
+
