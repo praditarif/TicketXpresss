0cedb4ae61d7f85510368b2d73c6bf22e6cdf6da
Attar <mattarannaufal@gmail.com>
2024-05-28 11:50:18 +0700

    feat: user login and registration logic

--- new-app/src/app/(navbar_layout)/login/page.tsx
+++ new-app/src/app/(navbar_layout)/login/page.tsx
@@ -1,9 +1,36 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import useSWRMutation from 'swr/mutation';
+
 import Button from '@/components/Button';
 import Input from '@/components/Input';
+import fetcher from '@/utils/fetcher';
 
 export default function Page() {
+  const router = useRouter();
+  const [error, setError] = useState(false);
+  const { data, trigger, isMutating } = useSWRMutation('/user/login', fetcher('POST'));
+
+  useEffect(() => {
+    if (data) {
+      if (data.code === 201) {
+        localStorage.setItem('request_token', data.payload.token);
+        router.push('/');
+      } else {
+        setError(data.message);
+      }
+    }
+  }, [data]);
+
+  const submitHandler = (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget;
+    trigger({ email: email.value, password: password.value });
+  }
+
   return (
     <main>
       <div className="flex justify-center items-center m-auto h-screen w-full object-cover" style={{ backgroundImage: 'url(/image/auth-background.avif)' }}>
@@ -15,16 +42,18 @@ export default function Page() {
               Selamat datang kembali! Masukkan akun lama anda agar kami mengingat Anda
             </p>
 
-            <form action="#">
+            <form onSubmit={submitHandler}>
               <div className="space-y-4 my-6">
+                {error && <p className="text-sm text-red-500">{error}</p>}
+                
                 <label htmlFor="email" className="sr-only">Email</label>
-                <Input type="email" placeholder="Enter email" className="w-full" required/>
+                <Input type="email" name="email" placeholder="Enter email" className="w-full" required />
 
                 <label htmlFor="password" className="sr-only">Password</label>
-                <Input type="password" className="w-full" placeholder="Password" required/>
+                <Input type="password" name="password" className="w-full" placeholder="Password" required />
               </div>
 
-              <Button variant="primary" type="submit" className="w-full rounded-lg text-lg">
+              <Button variant="primary" type="submit" className="w-full rounded-lg text-lg" disabled={isMutating}>
                 Login
               </Button>
             </form>
--- new-app/src/app/(navbar_layout)/registrasi/page.tsx
+++ new-app/src/app/(navbar_layout)/registrasi/page.tsx
@@ -1,7 +1,56 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import useSWRMutation from 'swr/mutation';
+
 import Button from '@/components/Button';
 import Input from '@/components/Input';
+import fetcher from '@/utils/fetcher';
 
 export default function Page() {
+  const router = useRouter();
+  const [error, setError] = useState(false);
+  const { data, trigger, isMutating } = useSWRMutation('/user/registration', fetcher('POST'));
+
+  useEffect(() => {
+    if (data) {
+      if (data.code === 201) {
+        toast.success('Pendaftaran berhasil, silahkan login');
+        setTimeout(() => router.push('/login'), 3000);
+      } else {
+        setError(data.message);
+      }
+    }
+  }, [data]);
+
+  const submitHandler = (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const {
+      nama_lengkap,
+      nama_panggilan,
+      telepon,
+      email,
+      jenis_identitas,
+      identitas,
+      password,
+      confirm_password,
+    } = event.currentTarget;
+
+    trigger({
+      nama_lengkap: nama_lengkap.value,
+      nama_panggilan: nama_panggilan.value,
+      telepon: telepon.value,
+      email: email.value,
+      jenis_identitas: jenis_identitas.value,
+      identitas: identitas.value,
+      password: password.value,
+      confirm_password: confirm_password.value,
+    });
+  }
+
   return (
     <div className="grid grid-cols-2 place-items-center h-dvh w-full bg-cover bg-no-repeat pt-16" style={{ backgroundImage: 'url(/image/auth-background.avif)' }}>
       <div className="bg-white col-start-2 w-fit h-fit rounded-xl p-8">
@@ -12,8 +61,10 @@ export default function Page() {
             Daftarlah sekarang untuk mulai menikmati layanan kami
           </p>
 
-          <form action="#">
+          <form onSubmit={submitHandler}>
             <div className="space-y-2.5 my-6">
+              {error && <p className="text-sm text-red-500">{error}</p>}
+
               <label className="sr-only" htmlFor="name">Nama Lengkap</label>
               <Input
                 className="w-full"
@@ -50,7 +101,7 @@ export default function Page() {
               <Input type="email" className="w-full" placeholder="Email" id="email" name="email" />
 
               <div className="flex gap-2.5">
-                <select name="jenis_identitas" className="bg-blue-500 rounded-lg text-white p-3 w-1/4" required>
+                <select name="jenis_identitas" className="bg-blue-500 rounded-lg text-white p-3 w-fit" required>
                   <option value="ktp">KTP</option>
                   <option value="passpor">Passpor</option>
                 </select>
@@ -87,8 +138,8 @@ export default function Page() {
               />
             </div>
 
-            <Button type="submit" className="w-full rounded-lg" variant="primary">
-              Registrasi
+            <Button type="submit" className="w-full rounded-lg" variant="primary" disabled={isMutating}>
+              {isMutating ? 'Loading...' : 'Registrasi'}
             </Button>
           </form>
         </div>
