bc4f2bbac201e8a8f9dd81bc1847ffde6fd612dd
Attar <mattarannaufal@gmail.com>
2024-05-28 11:07:43 +0700

    migrate: admin ticket CRD

--- new-app/src/app/admin/tiket/page.tsx
+++ new-app/src/app/admin/tiket/page.tsx
@@ -1,52 +1,59 @@
 'use client';
 
-import { IoClose, IoCreate } from 'react-icons/io5';
+import { useState } from 'react';
+import { IoClose } from 'react-icons/io5';
 import useSWR from 'swr';
 
+import ActionButton from '@/components/ActionButton';
 import Button from '@/components/Button';
 import Loading from '@/components/Loading';
 import Table from '@/components/Table';
+import AddForm from './AddForm';
 
 export default function Page() {
+  const [state, setState] = useState(false);
   const { data, isLoading } = useSWR('/admin/schedule/ticket');
 
   return (
-    <main className="p-10">
-      <header>
-        <h1 className="text-3xl font-bold">Daftar Tiket</h1>
-        <Button variant="primary" className="rounded-lg mt-4">Tambah</Button>
-      </header>
+    <>
+      <main className="p-10">
+        <header>
+          <h1 className="text-3xl font-bold">Daftar Tiket</h1>
+          <Button variant="primary" className="rounded-lg mt-4" onClick={() => setState(!state)}>Tambah</Button>
+        </header>
 
-      <section className="mt-8">
-        {isLoading ? (
-          <div className="h-[70dvh] w-full grid place-items-center">
-            <Loading />
-          </div>
-        ) : (
-          <Table
-            number
-            header={['Id', 'Kereta', 'Harga', 'Terjual', 'Tersisa', 'Aksi',]}
-            data={data.payload.map((data: any) => ({
-              id_tiket: data.id_tiket,
-              kereta: data.kereta,
-              harga: data.harga,
-              terjual: data.terjual,
-              sisa: data.sisa,
-              aksi: (
-                <div className="space-x-2">
-                  <Button variant="secondary" className="rounded-md aspect-square !p-1">
-                    <IoCreate className="text-lg" />
-                  </Button>
-
-                  <Button variant="secondary" className="rounded-md aspect-square !p-1 !border-red-500 text-red-500 bg-red-100">
+        <section className="mt-8">
+          {isLoading ? (
+            <div className="h-[70dvh] w-full grid place-items-center">
+              <Loading />
+            </div>
+          ) : (
+            <Table
+              number
+              header={['Id', 'Kereta', 'Harga', 'Terjual', 'Tersisa', 'Hapus',]}
+              data={data.payload.map((data: any) => ({
+                id_tiket: data.id_tiket,
+                kereta: data.kereta,
+                harga: data.harga,
+                terjual: data.terjual,
+                sisa: data.sisa,
+                aksi: (
+                  <ActionButton
+                    method="DELETE"
+                    revalidateRoute="/admin/schedule/ticket"
+                    routeAction={`/admin/schedule/${data.jadwal}/ticket/${data.id_tiket}`}
+                    className="border-red-500 text-red-500 bg-red-100"
+                  >
                     <IoClose className="text-lg" />
-                  </Button>
-                </div>
-              )
-            }))}
-          />
-        )}
-      </section>
-    </main>
+                  </ActionButton>
+                )
+              }))}
+            />
+          )}
+        </section>
+      </main>
+
+      <AddForm state={state} handler={setState} />
+    </>
   );
 }
